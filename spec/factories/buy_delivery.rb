e2e0743aa094d3b7a261218b1c36aa13816a6bfb
RinRintech <144243719+RinRintech@users.noreply.github.com>
2023-10-11 15:37:28 +0900

    購入のみのテストコード

---
+++ spec/factories/buy_delivery.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :buy_delivery do
+    postal_code { '123-4567' }
+    prefecture_id { 1 }
+    city { '中央区' }
+    addresses { '銀座' }
+    building { '' }
+    phone_number { '09012345678' }
+  end
+end
